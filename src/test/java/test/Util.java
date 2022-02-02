78288d1c510e868257f2de6a0775ec7fe54f7ced
KovacsicsTamas <tamas@wedoqa.co>
2022-02-02 15:46:43 +0100

    fixing util methods visibility

--- src/test/java/test/Util.java
+++ src/test/java/test/Util.java
@@ -8,14 +8,14 @@ public class Util{
 	static final String num = "0123456789";
 	static Random random = new Random();
 
-	String randomString(int len){
+	public String randomString(int len){
 	   StringBuilder sb = new StringBuilder(len);
 	   for(int i = 0; i < len; i++)
 	      sb.append(ab.charAt(random.nextInt(ab.length())));
 	   return sb.toString();
 	}
 	
-	String randomStringNumber(int len){
+	public String randomStringNumber(int len){
 		   StringBuilder sb = new StringBuilder(len);
 		   for(int i = 0; i < len; i++)
 		      sb.append(num.charAt(random.nextInt(num.length())));
